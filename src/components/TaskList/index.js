5933134ea1f6a40758562435d14568156f715838
Jalen Adams <jalen@jalenkadams.me>
2021-05-18 00:06:14 -0400

    Add: functionality for detecting change in task completion status

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { v5 as uuidv5 } from 'uuid';
 import AddTaskForm from './AddTaskForm';
+
+const TASK_UUID_NAMESPACE = 'b1a166c1-d556-4027-8717-56e7dcd702d5';
 
 /**
  * An item in the task list.
  *
  * @param {Object} props Props to pass to the component.
  * @param {Object} props.task An object containing task information.
+ * @param {function} props.onChange Callback function to execute when the checkbox is clicked.
  */
 const TaskListItem = (props) => {
-  const { task } = props;
+  const { task, onChange } = props;
   return (
     <li>
-      <input type="checkbox" checked={task.complete} />
+      <input type="checkbox" checked={task.complete} onChange={onChange} />
       {task.title}
     </li>
   );
@@ -20,6 +24,7 @@ const TaskListItem = (props) => {
 
 TaskListItem.propTypes = {
   task: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 /**
@@ -29,7 +34,7 @@ const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
   const createTask = (title) => {
-    return { title, complete: false };
+    return { title, id: uuidv5(title, TASK_UUID_NAMESPACE), complete: false };
   };
 
   const handleAdd = (title) => {
@@ -37,11 +42,30 @@ const TaskList = () => {
     setTasks(tasks.concat([newTask]));
   };
 
+  /**
+   * Updates a task's completion status when its checkbox is clicked.
+   *
+   * @param {Object} taskID The uuid of the task to update.
+   */
+  const handleChange = (taskID) => {
+    const items = [...tasks];
+    const targetIndex = items.findIndex((item) => {
+      return item.id === taskID;
+    });
+
+    const target = items[targetIndex];
+    target.complete = !target.complete;
+    items[targetIndex] = target;
+    setTasks(items);
+  };
+
   return (
     <div>
       <ul>
         {tasks.map((task) => {
-          return <TaskListItem task={task} />;
+          return (
+            <TaskListItem task={task} onChange={() => handleChange(task.id)} />
+          );
         })}
       </ul>
       <AddTaskForm onSubmit={handleAdd} />
