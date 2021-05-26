7047f0750abb885b907781f00cd77ad088b0a939
Jalen Adams <jalen@jalenkadams.me>
2021-05-26 10:07:11 -0400

    Add: style for sidebar

--- src/components/Sidebar/ProjectList.js
+++ src/components/Sidebar/ProjectList.js
@@ -1,37 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 
 import { ProjectContext } from '../Context';
 import ProjectListItem from './ProjectListItem';
 import AddProjectForm from './AddProjectForm';
 
-const StyledSidebarList = styled.ul`
+const StyledProjectList = styled.ul`
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
   list-style: none;
   margin: 0px;
   padding: 0px;
+  width: 100%;
 `;
 
 /**
  * A list of items to display in the sidebar.
  */
-const SidebarList = () => {
+const ProjectList = () => {
   // eslint-disable-next-line no-unused-vars
   const [projects, dispatchProjects] = useContext(ProjectContext);
+  const [selectedID, setSelectedID] = useState(projects[0].id);
 
   const listItems = projects.map((project) => {
-    return <ProjectListItem title={project.title} key={project.id} />;
+    return (
+      <ProjectListItem
+        title={project.title}
+        key={project.id}
+        selected={project.id === selectedID}
+        onClick={() => setSelectedID(project.id)}
+      />
+    );
   });
 
   return (
-    <StyledSidebarList>
-      <ul>
-        {listItems}
-        <li style={{ 'list-style': 'none' }}>
-          <AddProjectForm />
-        </li>
-      </ul>
-    </StyledSidebarList>
+    <StyledProjectList>
+      {listItems}
+      <li style={{ 'list-style': 'none' }}>
+        <AddProjectForm />
+      </li>
+    </StyledProjectList>
   );
 };
 
-export default SidebarList;
+export default ProjectList;
--- src/components/Sidebar/ProjectListItem.js
+++ src/components/Sidebar/ProjectListItem.js
@@ -3,12 +3,23 @@ import styled from 'styled-components';
 
 const StyledProjectListItem = styled.li`
   list-style: none;
+  text-align: left;
+  padding-left: 15px;
+  margin-bottom: 5px;
+  border: 10px solid ${(props) => (props.selected ? '#fcfcfa' : '#e98074')};
+  font-weight: bold;
+  background-color: ${(props) => (props.selected ? '#fcfcfa' : '#e98074')};
+  color: ${(props) => (props.selected ? '#e98074' : 'fcfcfa')};
 `;
 
 const ProjectListItem = (props) => {
-  const { title } = props;
+  const { title, selected, onClick } = props;
 
-  return <StyledProjectListItem>{title}</StyledProjectListItem>;
+  return (
+    <StyledProjectListItem onClick={onClick} selected={selected}>
+      {title}
+    </StyledProjectListItem>
+  );
 };
 
 export default ProjectListItem;
