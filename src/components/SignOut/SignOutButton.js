01018d9edbb97dc91a4a1ac8feb9aea4f57b7139
Jalen Adams <jalen@jalenkadams.me>
2021-05-30 15:33:21 -0400

    Add: redirect user to sign-in page after logging out

--- src/components/SignOut/SignOutButton.js
+++ src/components/SignOut/SignOutButton.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import Button from '../Button';
 import { withFirebase } from '../Context';
 
-const SignOutButtonBase = (props) => {
-  const { firebase } = props;
+import * as ROUTES from '../../constants/routes';
 
-  return <Button text="Sign Out" onClick={() => firebase.doSignOut()} />;
+const SignOutButtonBase = (props) => {
+  const { firebase, history } = props;
+
+  const handleClick = () => {
+    firebase.doSignOut();
+    history.push(ROUTES.SIGN_IN);
+  };
+
+  return <Button text="Sign Out" onClick={handleClick} />;
 };
 
-const SignOutButton = withFirebase(SignOutButtonBase);
+const SignOutButton = withRouter(withFirebase(SignOutButtonBase));
 
 export default SignOutButton;
